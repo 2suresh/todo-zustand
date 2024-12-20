4329864d25fa8c1ac467c3a97a727cb51d4a599b
SK <sureshkutti@gmail.com>
2024-12-21 01:48:25 +0530

    Adding more example like Counter Store

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
       <h1 className='my-4'>Todo List using Zustand</h1>
       <TodoForm />
       <TodoList />
+      <hr />
+      <span>Counter</span>
+      <Counter />
     </div>
   );
 }
---
+++ src/components/Counter.jsx
@@ -0,0 +1,42 @@
+import { useEffect } from 'react';
+import useCounterStore from '../store/counterStore';
+
+const logCount = () => {
+  const count = useCounterStore.getState().count;
+  console.log('count: ', count);
+};
+
+const setCounteState = () => {
+  useCounterStore.setState({ count: 100 });
+};
+
+const Counter = () => {
+  const count = useCounterStore((state) => state.count);
+  return <ChildComponent count={count} />;
+};
+
+// eslint-disable-next-line react/prop-types
+const ChildComponent = ({ count }) => {
+  //const increment = useCounterStore((state) => state.increment);
+  const incrementAsync = useCounterStore((state) => state.incrementAsync);
+  const decrement = useCounterStore((state) => state.decrement);
+
+  useEffect(() => {
+    //logCount();
+    setCounteState();
+  }, []);
+  return (
+    <div>
+      <p>{count}</p>
+      <button
+        onClick={incrementAsync}
+        className='mx-2'
+      >
+        Increment
+      </button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
